6a4a9b98f5905c6e897e71bd3cc796a26512128b
hakanhype <epic7058@gmail.com>
2026-07-31 11:17:21 +0300

    CI/CD pipeline ve observer iskeleti

---
+++ src/CiPilot.Core/Calculator.cs
@@ -0,0 +1,6 @@
+namespace CiPilot.Core;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+}
---
+++ tests/CiPilot.Core.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+using CiPilot.Core;
+using Xunit;
+
+namespace CiPilot.Core.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ReturnsSum()
+    {
+        var calc = new Calculator();
+        Assert.Equal(4, calc.Add(2, 2));
+    }
+
+    [Fact]
+    public void Add_WithNegativeNumber_ReturnsCorrectSum()
+    {
+        var calc = new Calculator();
+        Assert.Equal(1, calc.Add(3, -2));
+    }
+}
