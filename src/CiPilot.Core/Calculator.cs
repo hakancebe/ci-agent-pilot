a9d624c19dd0876f3ca11304d48f79c0dd2285fa
Hakan Cebe <161696076+hakancebe@users.noreply.github.com>
2026-09-03 13:43:19 +0300

    test: Faz 3 managed identity doğrulaması için kasıtlı hata

--- src/CiPilot.Core/Calculator.cs
+++ src/CiPilot.Core/Calculator.cs
@@ -3,4 +3,7 @@ namespace CiPilot.Core;
 public class Calculator
 {
     public int Add(int a, int b) => a + b;
+
+    // Faz 3 managed identity doğrulaması için KASITLI hata.
+    public int Subtract(int a, int b) => a - b
 }
