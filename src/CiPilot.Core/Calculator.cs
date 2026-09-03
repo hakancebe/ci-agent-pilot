ac5b6053f8f73df67c64953c9b3aa7a6a301f276
Hakan Cebe <161696076+hakancebe@users.noreply.github.com>
2026-09-03 17:04:15 +0300

    test: v0.3.4 + izleme son doğrulaması için kasıtlı hata

--- src/CiPilot.Core/Calculator.cs
+++ src/CiPilot.Core/Calculator.cs
@@ -3,4 +3,7 @@ namespace CiPilot.Core;
 public class Calculator
 {
     public int Add(int a, int b) => a + b;
+
+    // v0.3.4 + App Insights uçtan uca son doğrulama için KASITLI hata.
+    public int Modulo(int a, int b) => a % b
 }
