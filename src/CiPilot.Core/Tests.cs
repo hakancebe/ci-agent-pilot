590f93cf90e232c9e9cd144bf549ae57204cb4d2
hakanhype <epic7058@gmail.com>
2026-09-10 11:47:52 +0300

    bilinçli syntaxhatasitest

--- src/CiPilot.Core/Tests.cs
+++ src/CiPilot.Core/Tests.cs
@@ -5,14 +5,14 @@ using System.Threading.Tasks;
 // TEK SEFERDE SADECE 1 SENARYOYU AKTİF BIRAKIP DİĞERLERİNİ YORUM SATIRI YAP
 // Hepsi aynı anda açıksa çok sayıda hata birden basar (istersen öyle de test edebilirsin)
 
-// // 1) Syntax hatası (CS1002)
-// public class SyntaxHatasiTest
-// {
-//     public void Calistir()
-//     {
-//         Console.WriteLine("Merhaba")
-//     }
-// }
+// 1) Syntax hatası (CS1002)
+public class SyntaxHatasiTest
+{
+    public void Calistir()
+    {
+        Console.WriteLine("Merhaba")
+    }
+}
 
 // 2) Tip uyuşmazlığı (CS0029)
 // public class TipUyusmazligiTest
